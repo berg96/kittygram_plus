66b7bf2a2b194225adfb49bf7ecbabbb816cae0b
Artem Kulikov <chigar2010@yandex.ru>
2023-12-05 17:29:52 +0200

    add JWT

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,6 +1,5 @@
 from django.urls import include, path
 
-from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
 from cats.views import CatViewSet, OwnerViewSet
@@ -12,5 +11,9 @@ router.register('owners', OwnerViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token),
+    # Djoser создаст набор необходимых эндпоинтов.
+    # базовые, для управления пользователями в Django:
+    path('auth/', include('djoser.urls')),
+    # JWT-эндпоинты, для управления JWT-токенами:
+    path('auth/', include('djoser.urls.jwt')),
 ]
